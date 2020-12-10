bf286b9add593d8f3535c2e8b8bac1514997a30c
Alexey Ischenko <ishhenko.as@phystech.edu>
2020-12-10 07:35:18 +0300

    Initial Commit

---
+++ KnuthMorrisPratt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KnuthMorrisPratt
+//
+//  Created by Alexey Ischenko on 10.12.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
